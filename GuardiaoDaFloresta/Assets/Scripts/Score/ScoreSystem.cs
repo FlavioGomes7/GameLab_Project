bcdb179a21aaeb2e4d7706e8ededf3caaddd3e1e
FlavioGomes7 <flavio.gomes@estudante.ifms.edu.br>
2024-05-07 13:54:20 -0400

    Rating UI

--- GuardiaoDaFloresta/Assets/Scripts/Score/ScoreSystem.cs
+++ GuardiaoDaFloresta/Assets/Scripts/Score/ScoreSystem.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Events;
+using UnityEngine.UI;
 using UnityEngine;
 using System;
 
 public class ScoreSystem : MonoBehaviour
 {
     [SerializeField] private int score;
+    [SerializeField] private Image[] winStars;
+    [SerializeField] private Image[] loseStars;
     private bool wasWinned = false;
     private bool wasHitted = false;
     private bool wasDied = false;
@@ -53,18 +56,39 @@ public class ScoreSystem : MonoBehaviour
         else if (score >= 250 && score < 500)
         {
             Debug.Log("1 Star");
+            winStars[0].gameObject.SetActive(true);
+            loseStars[0].gameObject.SetActive(true);
             Debug.Log(score);
         }
         else if (score >= 500 && score < 1000)
         {
             Debug.Log("2 Star");
+            winStars[0].gameObject.SetActive(true);
+            loseStars[0].gameObject.SetActive(true);
+            winStars[1].gameObject.SetActive(true);
+            loseStars[1].gameObject.SetActive(true);
             Debug.Log(score);
         }
         else
         {
             Debug.Log("3 Star");
+            winStars[0].gameObject.SetActive(true);
+            loseStars[0].gameObject.SetActive(true);
+            winStars[1].gameObject.SetActive(true);
+            loseStars[1].gameObject.SetActive(true);
+            winStars[2].gameObject.SetActive(true);
+            loseStars[2].gameObject.SetActive(true);
             Debug.Log(score);
         }
     }
 
+    public void Start()
+    {
+        winStars[0].gameObject.SetActive(false);
+        loseStars[0].gameObject.SetActive(false);
+        winStars[1].gameObject.SetActive(false);
+        loseStars[1].gameObject.SetActive(false);
+        winStars[2].gameObject.SetActive(false);
+        loseStars[2].gameObject.SetActive(false);
+    }
 }
